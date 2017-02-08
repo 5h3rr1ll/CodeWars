a1b9254f444e7fd3868aa07abc900437fbf1a5f8
5h3rr1ll <a@sherrill.de>
2017-02-08 16:18:24 +0100

    few lines

--- TheMuseum.py
+++ TheMuseum.py
@@ -1,2 +1,15 @@
 #! /usr/bin/env python
 # -*- utf-8 -*-
+
+def remove_smallest(numbers):
+    if numbers == None:
+        raise NotImplementedError("TODO: remove_smallest")
+    else:
+        numbers.remove(min(numbers))
+    print(numbers)
+    # del(min(numbers))
+
+
+remove_smallest([1,2,3,4,5]) # [2,3,4,5]
+remove_smallest([5,3,2,1,4]) # [5,3,2,4]
+remove_smallest([2,2,1,2,1]) # [2,2,2,1]
