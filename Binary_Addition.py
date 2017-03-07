35f6025961a64e45a5cf8b119a7eb0b7fd42fe09
5h3rr1ll <a@sherrill.de>
2017-03-07 14:25:15 +0100

    Binary_Addition done

--- Binary_Addition.py
+++ Binary_Addition.py
@@ -1,12 +1,14 @@
 #! /usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def positive_sum(arr):
-    res = 0
+def add_binary(a,b):
+    """Returns the binary code of two numbers without the leading 0b and different
+    endings."""
+    # the ":b" within the {} does that the result of a + b become converted into binary
+    return "{:b}".format(a + b)
 
-    for x in arr:
-        if x > 0:
-            res += x
-            print(res)
-
-positive_sum([1,2,3,4,5])
+print(add_binary(1,1))
+print(add_binary(0,1))
+print(add_binary(1,0))
+print(add_binary(2,2))
+print(add_binary(51,12))
