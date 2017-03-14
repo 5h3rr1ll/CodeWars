f950c5582404749a9191f601f95934f7418234e9
5h3rr1ll <a@sherrill.de>
2017-03-14 14:12:37 +0100

    added comments and so on

--- Sum_of_odd_numbers.py
+++ Sum_of_odd_numbers.py
@@ -2,33 +2,29 @@
 # -*- coding: utf-8 -*-
 
 def row_sum_odd_numbers(n):
-    '''Die eingegebene Zahl gibt die Reihe in einem Dreieck wieder, das aus ung-
-    aden Zahlen besteht. In Jeder Reihe kommt eine Undgerade Zahl hinzu. In Reihe
-    1 ist also eine ungerade Zahl, in Reihe 2, zwei ungeraden Zahlen usw. Die Zahlen
-    einer Reihe werde addiert und von der Funktion zurückgegeben.'''
-
-    #Zu erstest benötige ich eine Liste die mir die Zahlen für jede Reiehe zusammenfasst
-
+    #need this to store a list of numbers which are odd
     lstOfOdds = []
 
+    #to store the number which tells me how many objects my list should have
     counter = 0
 
+    """ Pretty sure you can solce this for with fibbonacci, but I wans't able to do
+    it with fibbonacci, so this is my way to figure out, how many objects you will
+    need if your function get a valiu like 5 as an argument"""
     for i in range(n+1):
         counter += i
 
     number = 0
 
+    """As long as the list is shorter than the numbers of objectet needed it won't
+    stopp to increase number and check if it even or odd"""
     while len(lstOfOdds) <= (counter-1):
-
         if number % 2 != 0:
-
             lstOfOdds += [number]
             number += 1
         else:
-
             number += 1
-            
+    """ This step is about slicing. Slicing the list reverse."""
     return(sum(lstOfOdds[:-(n+1):-1]))
-
 
 row_sum_odd_numbers(3)
